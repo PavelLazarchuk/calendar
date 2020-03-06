715fcb928de32011d29628a093ca530a79412f05
PavelLazarchuk <pashalazarchuk@gmail.com>
2020-03-06 19:03:09 +0200

    add calendar page

--- src/shared/Radio/Radio.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Field } from 'formik';
-
-const Radio = ({ value, name }) => {
-	return <Field type="radio" name={name} value={value} />;
-};
-
-export default Radio;
--- src/utils/sortNumber.js
+++
@@ -1,5 +0,0 @@
-export default async function sortNumber(lists, sortName, bool) {
-	return bool
-		? await lists.sort((a, b) => b[sortName] - a[sortName])
-		: await lists.sort((a, b) => a[sortName] - b[sortName]);
-}
