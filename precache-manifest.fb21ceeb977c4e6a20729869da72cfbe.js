e362a7e3bb7fef7b43798ccf385b04d34c93e34b
PavelLazarchuk <pashalazarchuk@gmail.com>
2020-03-08 21:16:29 +0200

    Updates

---
+++ precache-manifest.fb21ceeb977c4e6a20729869da72cfbe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc125b2708420be335e687a6c8c6f3a6",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "5095d414250db3a65d51",
+    "url": "/calendar/static/css/2.8c8ea1a7.chunk.css"
+  },
+  {
+    "revision": "a4efe8ba627ba57e0be1",
+    "url": "/calendar/static/css/main.bc7e0f81.chunk.css"
+  },
+  {
+    "revision": "5095d414250db3a65d51",
+    "url": "/calendar/static/js/2.f806e13b.chunk.js"
+  },
+  {
+    "revision": "4214db9a6c23cd334176e62fe82579f5",
+    "url": "/calendar/static/js/2.f806e13b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4efe8ba627ba57e0be1",
+    "url": "/calendar/static/js/main.88d0f75a.chunk.js"
+  },
+  {
+    "revision": "25408d9b5016d7eb2c1b",
+    "url": "/calendar/static/js/runtime-main.c38e84c1.js"
+  }
+]);
